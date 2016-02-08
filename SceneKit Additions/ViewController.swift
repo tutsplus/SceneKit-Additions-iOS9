9ad02b573733ee40117a5eaaae751ef34ab60881
Davis Allie <davis525@live.com.au>
2016-02-09 10:53:08 +1100

    Completed project

--- SceneKit Additions/ViewController.swift
+++ SceneKit Additions/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import SceneKit
+import SpriteKit
 
 class ViewController: UIViewController {
+    
+    var sceneView: SCNView?
+    var mainScene = SCNScene(named: "MainScene.scn")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.sceneView = SCNView(frame: self.view.frame)
+        self.sceneView?.scene = mainScene
+        
+        self.view.addSubview(self.sceneView!)
+        
+        let button = UIButton(type: .System)
+        button.tintColor = UIColor.blueColor()
+        button.setTitle("Transition", forState: .Normal)
+        button.sizeToFit()
+        button.addTarget(self, action: "didPressTransition", forControlEvents: .TouchUpInside)
+        button.center.x = self.view.center.x
+        button.frame.origin.y = self.view.frame.height - button.frame.height - 12
+        self.sceneView?.addSubview(button)
+        
+        // MARK: Example Audio Node
+        let audioNode = SCNNode()
+        let audioSource = SCNAudioSource(fileNamed: "AnAudioFile.mp3")!
+        let audioPlayer = SCNAudioPlayer(source: audioSource)
+
+        audioNode.addAudioPlayer(audioPlayer)
+
+        let play = SCNAction.playAudioSource(audioSource, waitForCompletion: true)
+        audioNode.runAction(play)
+    }
+    
+    func didPressTransition() {
+        let transition = SKTransition.pushWithDirection(.Right, duration: 1.0)
+        self.sceneView?.presentScene(SCNScene(), withTransition: transition, incomingPointOfView: nil, completionHandler: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
